82ad05f5846d8ed0b10e35f6e422d4e0f3b08b1c
Linguistics <thylinguistics@gmail.com>
2021-01-09 15:03:45 -0800

    initial

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+<div class="input-group mb-3 w-50 m-auto">
+  <input type="text" class="form-control" placeholder="Enter a city" aria-label="Username" aria-describedby="basic-addon1">
+</div>
+</div>
+
+@endsection

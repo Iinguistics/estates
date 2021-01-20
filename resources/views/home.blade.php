25b66a047419e1f3024a98766d20b043acbab4c1
Linguistics <thylinguistics@gmail.com>
2021-01-19 16:37:30 -0800

    ...

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,9 +2,7 @@
 
 @section('content')
 <div class="container">
-<div class="input-group mb-3 w-50 m-auto">
-  <input type="text" class="form-control" placeholder="Enter a city" aria-label="Username" aria-describedby="basic-addon1">
-</div>
+testing
 </div>
 
 @endsection

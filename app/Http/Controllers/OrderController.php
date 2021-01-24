f084a1f587219391fb8a76f38ee5332bbe03f02c
Linguistics <thylinguistics@gmail.com>
2021-01-23 23:30:11 -0800

    added order controller methods

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\DB;
 use App\models\Order;
 
 
@@ -14,12 +16,32 @@ class OrderController extends Controller
    public function index(){
     //$orders = Order::all();
     $orders = Order::latest()->get();
-
-   return view('orders.index', [
+    return view('orders.index', [
    'orders'=>$orders,
    ]);
 
   }
+
+
+  public function store(){
+
+    // get an instance of the order table
+    $order = new Order();
+
+    // use request() to get the value from the form
+    $order->name = request('name');
+    $order->email = request('email');
+    $order->phone = request('phone');
+    $order->order = request('order');
+    
+   //$toppings = request('toppings')->json();
+   // $event->toppings = request($toppings);
+    // save event to data base...since $event is an instance of the model it has access to all the sql methods
+    $order->save();
+    
+    // use with() method to chain on to the redirect...session data
+    return redirect('/')->with('order-submit-message', 'Your order has been submitted');
+}
 
 
   public function show($id){
@@ -31,13 +53,17 @@ class OrderController extends Controller
   }
 
 
-  public function create(){
-    return view('orders.create');
+  
+
+  public function destroy($id){
+    $order = Order::findOrFail($id);
+    $order->delete();
+
+    return redirect('/orders')->with('order-destroy-message', 'Order has been deleted');
 }
 
-public function test(){
-    return view('private');
-}
+
+
 
 
 }
